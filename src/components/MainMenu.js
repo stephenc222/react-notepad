57096bb17c434dfcb79dc5ff518ba78efbc73b6f
Stephen Collins <Stephen Collins>
2017-03-01 21:44:12 -0600

    initial commit

---
+++ src/components/MainMenu.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MainMenu extends Component {
+  render () {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default MainMenu
---
+++ src/components/Notepad.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Notepad extends Component {
+  render () {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Notepad
