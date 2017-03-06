d3feb3aa34b7b60cf565bc21bc0e22ff03e78f29
Stephen Collins <Stephen Collins>
2017-03-06 17:05:49 -0600

    rearranged Component directory structure, taking a br eak from MainMenu and attempting to implement InputTextArea Component

---
+++ src/components/InputTextArea/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class InputTextArea extends Component {
+  render () {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default InputTextArea
